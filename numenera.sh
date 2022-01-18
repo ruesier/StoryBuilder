d3b3834fe064b3773f40110ac626a5a82466aed0
Devon Call <callbrentdevon@gmail.com>
2022-01-17 22:09:45 -0800

    allowed for default case

--- numenera.sh
+++ numenera.sh
@@ -1,19 +1,23 @@
 #! /bin/sh
 
-if [ "help" = $1 ]; then
-  echo "numenera.sh [num] [category]
+ALLCONFS="./conf/numenera.yaml:./conf/character.yaml:./conf/completion.yaml:./conf/danger.yaml:./conf/dice.yaml:./conf/location.yaml:./conf/name.yaml:./conf/util.yaml"
+
+HELPSTR="numenera.sh [num] [category]
 
 num =  number of generations
 
 category =
             'character' will generate a character idea
             'location' will generate a location idea
-            otherwise, will generate a campaign idea
-  "
-return 0
+            otherwise, will generate a campaign idea"
+
+if [ -n "$1" ]; then
+    if [ "help" = $1 ]; then
+        echo $HELPSTR
+        return 0
+    fi
 fi
 
-ALLCONFS="./conf/numenera.yaml:./conf/character.yaml:./conf/completion.yaml:./conf/danger.yaml:./conf/dice.yaml:./conf/location.yaml:./conf/name.yaml:./conf/util.yaml"
 INITCONF=""
 
 if [ "character" = "$2" ]; then
@@ -22,4 +26,8 @@ elif [ "location" = "$2" ]; then
   INITCONF="./conf/location.yaml:"
 fi
 
-buildstory -sb "$INITCONF$ALLCONFS" -r $1
+if [ -n "$1" ]; then
+    buildstory -sb "$INITCONF$ALLCONFS" -r $1
+else
+    buildstory -sb "$ALLCONFS"
+fi
