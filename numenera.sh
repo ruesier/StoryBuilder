13b22f0d91d40afb25e617a24b7658947f6b50d4
Devon Call <callbrentdevon@gmail.com>
2022-01-24 12:20:52 -0800

    added player concepts

--- numenera.sh
+++ numenera.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-ALLCONFS="./conf/numenera.yaml:./conf/character.yaml:./conf/completion.yaml:./conf/danger.yaml:./conf/dice.yaml:./conf/location.yaml:./conf/name.yaml:./conf/util.yaml"
+ALLCONFS="./conf/numenera.yaml:./conf/character.yaml:./conf/completion.yaml:./conf/danger.yaml:./conf/dice.yaml:./conf/location.yaml:./conf/name.yaml:./conf/util.yaml:./conf/player.yaml"
 
 HELPSTR="numenera.sh [num] [category]
 
